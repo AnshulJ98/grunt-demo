d5abdd2a18b853d492a72d7e08172333695f2137
Anshul Joshi <joshianshul98@yahoo.in>
2022-03-21 22:18:01 -0400

    !

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,30 @@
 module.exports = function(grunt){
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            combine: {
+                files: {
+                    'html/css/style.css': ['html/css/main.css', 'html/css/sidebar.css']
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'html/js/script.min.jss': ['html/js/script.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.registerTask("def", "", function(){
         grunt.log.write("YOYOYO");
     });
+
+    grunt.registerTask('default', ['cssmin','def','uglify']);
+
 };
